ef2b2eed6654a76bdded9db6283b530b304ae8dd
Ayan <ayanmullick@gmail.com>
2025-02-08 17:18:26 -0600

    PwdReset

--- MsEntra.Users.ps1
+++ MsEntra.Users.ps1
@@ -6,3 +6,10 @@ Get-EntraUser
 Set-EntraUser -UserId 17ecbbb8-6df0-4c76-85d9-1364d2e5658c -AccountEnabled $false -Verbose
 Get-EntraUser -UserId 17ecbbb8-6df0-4c76-85d9-1364d2e5658c|Select-Object accountEnabled
 #endregion
+
+
+#region reset password
+Connect-Entra -Scopes 'Directory.AccessAsUser.All'
+$securePassword = ConvertTo-SecureString '<>' -AsPlainText -Force
+Set-EntraUserPassword -ObjectId '05068d80-2789-41fd-92b0-fd5eb32ec8fc' -Password $securePassword -Verbose
+#endregion
