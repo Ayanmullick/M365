b11d4bdee237d2a79cfb79f6d043307890cd7789
Ayan <ayanmullick@gmail.com>
2025-03-25 10:37:40 -0500

    Groups

---
+++ MsEntra.Groups.ps1
@@ -0,0 +1,9 @@
+Connect-Entra -Scopes 'User.ReadWrite.All', 'Directory.AccessAsUser.All' -Verbose
+New-EntraGroup -DisplayName '<DisplayName>' -MailEnabled $false -SecurityEnabled $true -MailNickname "<>" -Description "<>" -Verbose
+$Group = Get-EntraGroup -Filter "mailNickname eq 'DisplayName'"
+
+$emails = @('Ayan.Mullick@<>', '<>@<>','<>@<>','<>@<>','<>@<>')
+     foreach ($email in $emails) {
+         $user = Get-EntraUser -Filter "mail eq '$email'"
+         Add-EntraGroupMember -GroupId $group.Id -RefObjectId $user.Id
+     }
